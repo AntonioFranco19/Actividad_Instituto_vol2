dde2bdbce1515f559659af4859506542e388a0fe
Antonio Franco Cegarra <a.francocegarra00@gmail.com>
2026-04-18 12:06:42 +0200

    Se ha terminado la mision 3, se ha copletado la actividad

--- TecnoFuturo.Core/Entities/Modulo.cs
+++ TecnoFuturo.Core/Entities/Modulo.cs
@@ -1,21 +1,23 @@
+using MessagePack;
 using TecnoFuturo.Core.Helpers;
 
 namespace TecnoFuturo.Core.Entities;
 
+[MessagePackObject]
 public class Modulo : IInfoDetallada
 {
-    public string CicloFormativoId { get; set; } = null!;
-    public int ModuloId { get; set; }
-    public string? Nombre { get; set; }
-    public int Horas { get; set; }
-    public string? ProfesorNif { get; set; }
+    [Key(0)] public string CicloFormativoId { get; set; } = null!;
+    [Key(1)] public int ModuloId { get; set; }
+    [Key(2)] public string? Nombre { get; set; }
+    [Key(3)] public int Horas { get; set; }
+    [Key(4)] public string? ProfesorNif { get; set; }
 
     public string ObtenerFicha()
     {
         return $"""
-                 CODIGO .: {ModuloId}
-                 NOMBRE .: {Nombre}
-                 HORAS ..: {Horas:N0}
-                 """;
+                CODIGO .: {ModuloId}
+                NOMBRE .: {Nombre}
+                HORAS ..: {Horas:N0}
+                """;
     }
 }
